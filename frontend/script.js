59e78f0ba90cf2bb9d98ee720ddf97db439fbcda
Samuel Taylor <github@samueltaylor.org>
2017-04-29 12:34:02 -0500

    Center the map

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var mymap = L.map('map').setView([51.505, -0.09], 2);
+  var mymap = L.map('map').setView([0, 0], 2);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
